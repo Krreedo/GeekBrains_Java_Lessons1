22e2cad6f76af9ff26e3163cd31945412be6b81d
Sofronov_Oleg <sofronov.ov94@gmail.com>
2020-08-09 00:41:01 +0300

    Start HW7

---
+++ HW7/src/ru/geekbrains/Java_1/Cat.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.Java_1;
+
+public class Cat {
+    private String name;
+
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat(Plate plate) {
+        System.out.println(name + " eat");
+        plate.decreaseFood(10);
+    }
+    }
+
